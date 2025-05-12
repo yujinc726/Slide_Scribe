8aacaf8a011609927ac78f21481efe244c68b6d7
yujinc726 <yujinc726@gmail.com>
2025-05-13 06:29:20 +0900

    Update transcriber.py

--- transcriber.py
+++ transcriber.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def transcriber_tab():
-    st.markdown(f'[Google Colab](https://colab.research.google.com/drive/14FvsarkxDMjGCudHym-ttJy0qzwKu41o?usp=sharing)')
-    st.text_area("Code", value="!git clone https://github.com/yujinc726/Whisper-AI-with-Gradio.git\n%cd Whisper-AI-with-Gradio\n!pip install -r requirements.txt\n%run main.py", height=200, label_visibility="collapsed")
+    st.markdown(f'[Google Colaboratory](https://colab.research.google.com/drive/14FvsarkxDMjGCudHym-ttJy0qzwKu41o?usp=sharing)')
+    st.text_area(f"[Google Colaboratory](https://colab.research.google.com/drive/14FvsarkxDMjGCudHym-ttJy0qzwKu41o?usp=sharing)", value="!git clone https://github.com/yujinc726/Whisper-AI-with-Gradio.git\n%cd Whisper-AI-with-Gradio\n!pip install -r requirements.txt\n%run main.py", height=100)
